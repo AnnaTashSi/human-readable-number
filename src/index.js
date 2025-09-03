1eccb69c22f2495bafad89a73f98a292f95f075f
AnnaTashSi <136920681+AnnaTashSi@users.noreply.github.com>
2025-09-03 21:56:19 +0300

    Fix: return deleted comments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-module.exports = function toReadable(numb) {
+module.exports = function toReadable(num) {
   const ones = [
     '',
     'one',
@@ -38,12 +38,18 @@ module.exports = function toReadable(numb) {
   ];
 
   function convertTens(numb) {
+    // Function for tens (20-99)
     if (numb < 10) return ones[numb];
-    
+    // If parameter is less than 10, return text 1-9
     if (numb >= 10 && numb < 20) return teens[numb - 10];
+    // If parameter is 10-19, return text 10-19
+    // [number - 10] calculates position in array
 
     return `${tens[Math.floor(numb / 10)]} ${ones[numb % 10]}`.trim();
+    // If parameter >=20, return text 20-99
+    // calculate array position (20/10=2) + space + remainder position/10-- 20/10=2, i.e. 0)
   } 
+  // doesn't work on 20,30,40,50,60,70,80,90,100 without trim() method - removes trailing space
 
   function convertHundreds(numb) {
     if (numb > 99 && numb < 1000) {
@@ -53,11 +59,13 @@ module.exports = function toReadable(numb) {
     }
     return convertTens(numb);
   } 
+  // Function for hundreds. if parameter is 100-999..........
 
   function convert(numb) {
     if (numb === 0) return 'zero';
     return convertHundreds(numb);
   } 
+  // Function for 0. if parameter == 0, then return text, otherwise call hundreds function
 
-  return convert(numb); 
+  return convert(num); // call function for 0
 };
